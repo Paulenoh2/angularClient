d874197f4a8bcb1d6de62de9aa072372063ba4b7
paulenoh2 <paulenoh2@gmail.com>
2022-12-07 19:39:07 +0200

    tables details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'pvi-registration';
+export class AppComponent implements OnInit {
+  title:string = 'Pvi Registration';
+  students: any[] = [];
+
+fakeArray = [1,2,3,4,5,6,6,7,8,9]
+constructor(){
+}
+
+  ngOnInit(): void{
+console.log("HI How are you ")
+  this.getStudents();
+  }
+
+getStudents(): void{
+console.log("I am getStudents")
+
+   this.students = [
+      {
+      id: 1,
+      idNumber: 1455,
+      phoneNumber: 1455,
+      firstName: "Sam",
+      lastName: "Ben",
+      email: "ben@gmail.com"
+      },
+      {
+            id: 2,
+            idNumber: 458,
+            phoneNumber: 1455,
+            firstName: "Sa23",
+            lastName: "Ben2",
+            email: "ben2@gmail.com"
+            },
+            {
+                        id: 3,
+                        idNumber: 55,
+                        phoneNumber: 1455,
+                        firstName: "Sa2333",
+                        lastName: "Ben2",
+                        email: "ben3@gmail.com"
+                        }
+    ];
+  }
 }
